b50895cadbb4ae7e7d47aa4efa1e8c6095649d2a
chousa18 <chousa18@b048-27.crio.dauphine.fr>
2019-10-18 12:59:17 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/ChoubaneSamia/td00/PrimeCollection.java
@@ -0,0 +1,46 @@
+package fr.dauphine.ja.ChoubaneSamia.td00;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class PrimeCollection
+{
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	
+	
+
+
+    public PrimeCollection(ArrayList<Integer> numbers) {
+		
+		this.numbers = numbers;
+	}
+    
+    public void  initRandom (int n,int m) {
+    	
+    }
+
+
+    public boolean isPrime(int p) {
+    
+    	return  true ; 
+    }
+
+    
+    public ArrayList<Integer> printPrimes() {
+    	ArrayList  L = null;
+    	
+    	return  L;
+    	
+    }
+
+	public static void main( String[] args )
+    {
+
+ 	
+    }
+    
+    
+}
