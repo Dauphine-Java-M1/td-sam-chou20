3aeba45b71f4ebcaafbd4ffe3c687ad47e2ac465
chousa18 <chousa18@b028-28.crio.dauphine.fr>
2019-10-24 15:14:28 +0200

    test 1

--- td00/src/main/java/fr/dauphine/ja/ChoubaneSamia/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/ChoubaneSamia/td00/PrimeCollection.java
@@ -2,44 +2,15 @@ package fr.dauphine.ja.ChoubaneSamia.td00;
 
 import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
 public class PrimeCollection
 {
-	private ArrayList<Integer> numbers = new ArrayList<Integer>();
-	
-	
 
-
-    public PrimeCollection(ArrayList<Integer> numbers) {
-		
-		this.numbers = numbers;
-	}
-    
-    public void  initRandom (int n,int m) {
-    	
-    }
-
-
-    public boolean isPrime(int p) {
-    
-    	return  true ; 
-    }
-
-    
-    public ArrayList<Integer> printPrimes() {
-    	ArrayList  L = null;
-    	
-    	return  L;
-    	
-    }
 
 	public static void main( String[] args )
     {
 
 		System.out.println(" hello ");
+		System.out.println(" hello hello");
  	
     }
     
