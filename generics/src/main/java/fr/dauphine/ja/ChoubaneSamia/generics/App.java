9368d00ab9654d092cd18e3b304cec753b48bdee
chousa18 <chousa18@b032-26.crio.dauphine.fr>
2019-11-29 13:59:59 +0100

    Ajout generics

---
+++ generics/src/main/java/fr/dauphine/ja/ChoubaneSamia/generics/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.ChoubaneSamia.generics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
